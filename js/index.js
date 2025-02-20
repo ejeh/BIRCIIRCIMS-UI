134cd0668619abed2805ba08b799653cfd0776e7
ejeh <ejehgodfrey@gmail.com>
2025-02-20 10:42:27 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const BACKEND_URL = "http://localhost:5000/api";
-// const BACKEND_URL = "https://bscr-mis.onrender.com/api";
+// const BACKEND_URL = "http://localhost:5000/api";
+const BACKEND_URL = "https://bscr-mis-ui.onrender.com/api";
 
 let userData = localStorage.getItem("token");
 userData = JSON.parse(userData);
