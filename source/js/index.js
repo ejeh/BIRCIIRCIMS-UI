f9de152c3bea36c2435e834787cf3ba90c6b1c39
ejeh <ejehgodfrey@gmail.com>
2025-06-29 17:24:46 -0700

    New commit

--- source/js/index.js
+++ source/js/index.js
@@ -2,12 +2,11 @@ const isDevelopment = window.location.hostname === "127.0.0.1";
 
 // const BACKEND_URL = isDevelopment
 //   ? "http://localhost:5000/api"
-//   : "http://api.citizenship.benuestate.gov.ng/api";
+//   : "https://api.citizenship.benuestate.gov.ng/api";
 
 const BACKEND_URL = "https://api.citizenship.benuestate.gov.ng/api"
-console.log(BACKEND_URL)
-  // ? "http://api.citizenship.benuestate.gov.ng/api"
-  // : "http://localhost:5000/api"
+
+
 
 const FRONTEND_URL = isDevelopment
   ? "http://127.0.0.1:5501"
